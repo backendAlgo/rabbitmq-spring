fb4294aa0116a641737f7d3e1b371f0848fa392d
Mirshod <mirshod.mirjonov@mail.ru>
2021-04-27 01:15:30 +0500

    fixedrate with multiple consumers

--- rabbitmq.producer/src/main/java/com/course/rabbitmq/producer/Application.java
+++ rabbitmq.producer/src/main/java/com/course/rabbitmq/producer/Application.java
@@ -5,17 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application implements CommandLineRunner {
-	@Autowired
-	private HelloRabbitProducer producer;
+//	@Autowired
+//	private HelloRabbitProducer producer;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		producer.sendHello("Mirshodjon Mirjonov " + Math.random());
+//		producer.sendHello("Mirshodjon Mirjonov " + Math.random());
 	}
 }
